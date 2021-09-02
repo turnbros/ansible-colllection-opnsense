6039796b21509be41aaec1a57234a0edb49a25a5
Dylan Turnbull <dturbull@seismic.com>
2021-09-02 11:03:04 -0500

    first working version

--- plugins/modules/firewall_alias.py
+++ plugins/modules/firewall_alias.py
@@ -5,6 +5,8 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import json
+
 DOCUMENTATION = r'''
 ---
 module: firewall_alias
@@ -38,11 +40,6 @@ options:
         description: A path to the CA files used to validate the Opnsense cert
         required: false
         type: str
-    parent:
-        description: Name of other alias who will contain this alias (nested)
-        required: false
-        type: str
-        default: None
     name:
         description: Name of the alias
         required: true
@@ -66,6 +63,11 @@ options:
         required: false
         type: bool
         default: true
+    state:
+        description: Determines wether or not this alias should exist.
+        required: false
+        type: str
+        default: present
 author:
     - Your Name (@yourGitHubHandle)
 '''
@@ -73,7 +75,6 @@ author:
 EXAMPLES = r'''
 - name: Create new alias
   turnbros.opnsense.firewall_alias:
-    parent: some_other_alias
     name: hello_world
     description: a description for your alias
     type: port
@@ -81,6 +82,7 @@ EXAMPLES = r'''
         - 42
         - another_port_alias
     enabled: true
+    state: present
 '''
 
 RETURN = r'''
@@ -107,76 +109,165 @@ my_useful_info:
 
 from ansible.module_utils.basic import AnsibleModule
 from opnsense_api import Opnsense
+from opnsense_api.util import AliasType
 
 def run_module():
 
-    module_args = dict(
-        api_key=dict(type='str', required=True),
-        api_secret=dict(type='str', required=True, no_log=True),
-        api_scheme=dict(type='str', required=False, default="https"),
-        api_host=dict(type='str', required=True),
-        api_port=dict(type='int', required=False, default=443),
-        api_ca_path=dict(type='str', required=False, default=None),
-        parent=dict(type='str', required=False, default=None),
-        name=dict(type='str', required=True),
-        description=dict(type='str', required=False, default=None),
-        type=dict(type='str', required=False, choices=['host', 'network', 'port', 'url'], default=None),
-        content=dict(type='list', required=False, default=None),
-        enabled=dict(type='bool', required=False, default=None),
+  module_args = dict(
+    api_key=dict(type='str', required=True),
+    api_secret=dict(type='str', required=True, no_log=True),
+    api_scheme=dict(type='str', required=False, default="https"),
+    api_host=dict(type='str', required=True),
+    api_port=dict(type='int', required=False, default=443),
+    api_ca_path=dict(type='str', required=False, default=None),
+
+    name=dict(type='str', required=True),
+    description=dict(type='str', required=False, default=""),
+    type=dict(type='str', required=True, choices=['host', 'network', 'port', 'url']),
+    content=dict(type='list', required=False, default=[]),
+    enabled=dict(type='bool', required=False, default=True),
+    state=dict(type='str', required=False, choices=['present', 'absent'], default='present'),
+  )
+
+  result = dict(
+    changed=False,
+    alias=dict(),
+    diff=dict(
+      before=dict(),
+      after=dict()
     )
+  )
 
-    result = dict(
-        changed=False,
-        alias={},
-        diff={
-            'before': '',
-            'after': '',
-        }
-    )
+  module = AnsibleModule(
+    argument_spec=module_args,
+    # TODO: This should really support checkmode. I just don't have time right now.
+    supports_check_mode=True
+  )
 
-    module = AnsibleModule(
-        argument_spec=module_args,
-        supports_check_mode=True
-    )
+  # Open a connection to the Opnsense device and return the alias controller
+  alias_controller = Opnsense(api_key=module.params['api_key'],
+                              api_secret=module.params['api_secret'],
+                              scheme=module.params['api_scheme'],
+                              host=module.params['api_host'],
+                              port=module.params['api_port'],
+                              ca_path=module.params['api_ca_path']
+                              ).firewall.alias_controller
 
-    if module.check_mode:
-        module.exit_json(**result)
+  # Get all the module params
+  alias_name = module.params['name']
+  alias_uuid = alias_controller.get_alias_uuid(alias_name)
+  alias_description = module.params['description']
+  alias_type = module.params['type']
+  alias_content = module.params['content']
+  alias_enabled = bool(int(module.params['enabled']))
+  alias_state = module.params['state']
 
-    alias_controller = Opnsense(api_key=module_args['api_key'],
-                                api_secret=module_args['api_secret'],
-                                scheme=module_args['api_scheme'],
-                                host=module_args['api_host'],
-                                port=module_args['api_port'],
-                                ca_path=module_args['api_ca_path']
-                                ).firewall.alias_controller
+  # Get the alias if the get_alias_uuid call returned a value
+  alias = None
+  if alias_uuid is not None:
+    alias = alias_controller.get_alias(alias_uuid)
 
-    alias_parent = module_args['parent']
-    alias_name = module_args['name']
-    alias_description = module_args['description']
-    alias_type = module_args['type']
-    alias_content = module_args['content']
-    alias_enabled = module_args['enabled']
+  #################################
+  ##       Delete Alias          ##
+  #################################
+  if alias_state == "absent":
+    if alias_uuid is not None:
+      if not module.check_mode:
+        alias_controller.delete_alias(alias_uuid)
 
-    alias_controller.get_alias_uuid()
-
-    # manipulate or modify the state as needed (this is going to be the
-    # part where your module will do what it needs to do)
-    result['original_message'] = module.params['name']
-    result['message'] = 'goodbye'
-    result['my_useful_info'] = {
-        'foo': 'bar',
-        'answer': 42,
-    }
-    # in the event of a successful module execution, you will want to
-    # simple AnsibleModule.exit_json(), passing the key/value results
-
-
+      result["changed"] = True
+      result["alias"] = alias
+      result["diff"]["before"] = alias
+      result["diff"]["after"] = dict()
 
     module.exit_json(**result)
 
+  if alias_state == "present":
+
+    #################################
+    ##       Create Alias          ##
+    #################################
+    if alias_uuid is None:
+      if not module.check_mode:
+        response = alias_controller.add_alias(
+          name=alias_name,
+          alias_type=AliasType[alias_type.upper()],
+          description=alias_description,
+          content=alias_content,
+          enabled=alias_enabled
+        )
+        if (response['result'] == 'saved') and ('uuid' in response):
+          alias = alias_controller.get_alias(response['uuid'])
+        else:
+          module.fail_json(f"Failed to create alias with response: {json.dumps(response)}")
+
+      else:
+        alias = dict(
+          name=alias_name,
+          alias_type=alias_type,
+          description=alias_description,
+          content=alias_content,
+          enabled=alias_enabled
+        )
+
+      result["changed"] = True
+      result["alias"] = alias
+      result["diff"]["before"] = dict()
+      result["diff"]["after"] = alias
+
+    #################################
+    ##       Update Alias          ##
+    #################################
+    else:
+
+      # Figure out if we need to update the alias
+      update_needed = False
+      if (
+        (alias['type'] != alias_type.upper())
+        or (alias['description'] != alias_description)
+        or (alias['content'] != [str(item) for item in alias_content])
+        or (alias['enabled'] != alias_enabled)
+      ): update_needed = True
+
+      # Setting this here so the editor stops yelling at me about
+      # using it before its set.
+      updated_alias = alias
+
+      # Make the change via the API, but ONLY if actually needed.
+      if not module.check_mode and update_needed :
+        response = alias_controller.set_alias(
+          uuid=alias_uuid,
+          name=alias_name,
+          alias_type=AliasType[alias_type.upper()],
+          description=alias_description,
+          content=alias_content,
+          enabled=alias_enabled
+        )
+
+        if response['result'] == 'saved':
+          updated_alias = alias_controller.get_alias(alias_uuid)
+        else:
+          module.fail_json(f"Failed to update the alias with response: {json.dumps(response)}")
+      else:
+        updated_alias = dict(
+          uuid=alias_uuid,
+          name=alias_name,
+          alias_type=alias_type.upper(),
+          description=alias_description,
+          content=alias_content,
+          enabled=alias_enabled
+        )
+
+      result["changed"] = update_needed
+      result["alias"] = updated_alias
+      result["diff"]["before"] = alias
+      result["diff"]["after"] = updated_alias
+
+  module.exit_json(**result)
+
 def main():
-    run_module()
+  run_module()
 
 
 if __name__ == '__main__':
-    main()
+  main()
